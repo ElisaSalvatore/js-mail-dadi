b74b7375811168678178ced5818d5ce584d315cc
ElisaSalvatore <elisa.salvatore110@gmail.it>
2021-12-13 22:19:57 +0100

    add: outputHtml, fix: Math.random

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const outputHtml = document.getElementById("output");
+
 // ESERCIZIO MAIL
 //Definisco array di chi può effettuare l'accesso
 const listEmail = ['mariorossi@gmail.com', 'marioverdi@gmail.com', 'gianniblu@gmail.com'];
@@ -12,23 +14,31 @@ for (let i = 0; i < listEmail.length; i++) {
 }
 //Stampiamo il resoconto dell'accesso 
 if (flag) {
-    console.log('Accesso effettuato con successo!')
+    outputHtml.innerHTML += "Accesso effettuato con successo! </br>";
+    // console.log('Accesso effettuato con successo!')
 } else {
-    console.log('Impossibile effettuare l\'accesso.')
+    outputHtml.innerHTML += "Impossibile effettuare l'accesso. </br>";
+    // console.log('Impossibile effettuare l\'accesso.')
 }
 
 // ESERCIZIO DADI
-//Generare numero random utente
+//1. Generare numero random utente
 const humanNumber = Math.floor(Math.random() * 6) + 1;
-console.log('Numero utente: ' + humanNumber);
-//Generare numero random computer
+outputHtml.innerHTML += "Numero utente: " + humanNumber;
+//console.log('Numero utente: ' + humanNumber);
+
+//2. Generare numero random computer
 const computerNumber = Math.floor(Math.random() * 6) + 1;
-console.log('Numero computer: ' + computerNumber);
+outputHtml.innerHTML += "</br> Numero computer: " + computerNumber;
+//console.log('Numero computer: ' + computerNumber);
 
 if (humanNumber < computerNumber) {
-    console.log('Hai perso.')
+    outputHtml.innerHTML += "</br> Hai perso.";
+    //console.log('Hai perso.')
 } else if (humanNumber > computerNumber) {
-    console.log('Hai vinto!')
+    outputHtml.innerHTML += "</br> Hai vinto!";
+    //console.log('Hai vinto!')
 } else {
-    console.log('Pareggio.')
+    outputHtml.innerHTML += "</br> Pareggio.";
+    //console.log('Pareggio.')
 }
