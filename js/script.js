eb9008799248e200fc451105b914cbe32504e9c1
ElisaSalvatore <elisa.salvatore110@gmail.it>
2021-12-13 16:32:11 +0100

    add: toLowerCase( )

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // ESERCIZIO MAIL
 //Definisco array di chi può effettuare l'accesso
-const listEmail = ['mariorossi@gmail.com', 'marioverdi@gmail.com', 'gianniblu@gmailcom'];
+const listEmail = ['mariorossi@gmail.com', 'marioverdi@gmail.com', 'gianniblu@gmail.com'];
 let flag = false;
 //Chiedo all'utente la propria mail
 const userEmail = prompt('Inserisci la tua email:');
 //Ciclo for all'interno dell'array per verificarne l'accesso
 for (let i = 0; i < listEmail.length; i++) {
-    if (userEmail === listEmail[i]) {
+    if (userEmail.toLowerCase() === listEmail[i].toLowerCase()) {
         flag = true;
     }
 }
