b36d3119611b2b08ad4686463f8f06af67a14488
ElisaSalvatore <elisa.salvatore110@gmail.it>
2021-12-14 10:23:26 +0100

    add: method includes() fix: Math.random()

--- js/script.js
+++ js/script.js
@@ -2,35 +2,54 @@ const outputHtml = document.getElementById("output");
 
 // ESERCIZIO MAIL
 //Definisco array di chi può effettuare l'accesso
-const listEmail = ['mariorossi@gmail.com', 'marioverdi@gmail.com', 'gianniblu@gmail.com'];
-let flag = false;
+const listEmails = ['mariorossi@gmail.com', 'marioverdi@gmail.com', 'gianniblu@gmail.com'];
 //Chiedo all'utente la propria mail
 const userEmail = prompt('Inserisci la tua email:');
+
+/*
+// 1^ SOLUZIONE => FOR
+let emailTrovata = false;
 //Ciclo for all'interno dell'array per verificarne l'accesso
-for (let i = 0; i < listEmail.length; i++) {
-    if (userEmail.toLowerCase() === listEmail[i].toLowerCase()) {
-        flag = true;
+for (let i = 0; i < listEmails.length; i++) {
+    if (userEmail.toLowerCase() === listEmails[i].toLowerCase()) {
+        emailTrovata = true;
     }
 }
 //Stampiamo il resoconto dell'accesso 
-if (flag) {
+if (emailTrovata) {
     outputHtml.innerHTML += "Accesso effettuato con successo! </br>";
     // console.log('Accesso effettuato con successo!')
 } else {
     outputHtml.innerHTML += "Impossibile effettuare l'accesso. </br>";
     // console.log('Impossibile effettuare l\'accesso.')
 }
+*/
+
+
+//2^ SOLUZIONE => INCLUDES
+// includes torna un booleano (TRUE|FALSE)
+if (listEmails.includes(userEmail)) {
+    console.log('Email trovata con includes!')
+} else {
+    console.log('Email NON trovata con includes!')
+}
+ 
 
 // ESERCIZIO DADI
+// Un formula generale che genera un numero random tra un min e max, compresi gli estremi.
+// Math.floor(Math.random() * ((max + 1) - min)) + min => include minimo e massimo. Funziona anche nel caso in cui il mino non sia 1.
+// Math.round(Math.random() * (max - min)) + min;
+
 //1. Generare numero random utente
-const humanNumber = Math.floor(Math.random() * 6) + 1;
+const humanNumber = Math.floor(Math.random() * (6 - 1)) + 1;
 outputHtml.innerHTML += "Numero utente: " + humanNumber;
-//console.log('Numero utente: ' + humanNumber);
 
 //2. Generare numero random computer
-const computerNumber = Math.floor(Math.random() * 6) + 1;
+const computerNumber = Math.floor(Math.random() * (6 - 1)) + 1;
 outputHtml.innerHTML += "</br> Numero computer: " + computerNumber;
-//console.log('Numero computer: ' + computerNumber);
+
+console.log ('Numero utente:', humanNumber, 'Numero computer:', computerNumber);
+
 
 if (humanNumber < computerNumber) {
     outputHtml.innerHTML += "</br> Hai perso.";
